87f983be85b0fa227062d285f09d25ddec228905
HajinPark <gkwls0812@naver.com>
2018-07-27 13:13:48 +0900

    페이스북

--- app/src/main/java/and/harim/com/cocktail/MenuActivity.java
+++ app/src/main/java/and/harim/com/cocktail/MenuActivity.java
@@ -1,7 +1,10 @@
 package and.harim.com.cocktail;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void click_facebook(View view) {
+        Intent myIntent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/%ED%98%9C%ED%99%94-%EC%B9%B5%ED%85%8C%EC%9D%BC-600220950378118/?modal=admin_todo_tour"));
+        startActivity(myIntent);
     }
 }
