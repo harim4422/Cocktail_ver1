e52a32abc48b4fa6145a43094e0473a9991b0dd7
harim4422 <harim4422@naver.com>
2018-07-27 17:40:43 +0900

    헬프 레이아웃 수정, 헬프 버튼 연결, 세팅버튼 추가 및 연결 //하림

--- app/src/main/java/and/harim/com/cocktail/MenuActivity.java
+++ app/src/main/java/and/harim/com/cocktail/MenuActivity.java
@@ -1,7 +1,9 @@
 package and.harim.com.cocktail;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,19 +24,34 @@ public class MenuActivity extends AppCompatActivity {
     public void click_bar(View view) {
         Intent bar = new Intent(this,BarActivity.class);
         startActivity(bar);
-    }
+    }//맛집 버튼 연결
 
     public void click_cocktail(View view) {
         Intent cocktail = new Intent(this,CocktailActivity.class);
         startActivity(cocktail);
-    }
+    }//칵테일 버튼 연결
 
     public void click_recipe(View view) {
-    }
+    }//나만의 요리법 연결
 
     public void click_set(View view) {
-    }
+        Intent set = new Intent(this,SettingsActivity.class);
+        startActivity(set);
+    }//세팅 연결
 
     public void click_help(View view) {
-    }
+        Intent help = new Intent(this,HelpActivity.class);
+        startActivity(help);
+    }//도움말 연결
+
+    public void click_back(View view) {
+        Intent home = new Intent(this, MainActivity.class);
+        startActivity(home);
+    }//하단 뒤로가기 버튼 (종료)
+
+    public void click_home(View view) {
+        Intent home = new Intent(this, MainActivity.class);
+        startActivity(home);
+    }//하단 홈 버튼
+
 }
